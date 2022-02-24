cc254a15cee6b81356e556a98066dd7ab45310db
bakroli <bak.roland@hotmail.com>
2022-02-24 16:30:44 +0100

    Finish

---
+++ src/main/java/hu/nive/ujratervezes/wordcounter/Main.java
@@ -0,0 +1,12 @@
+package hu.nive.ujratervezes.wordcounter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "abc dfg.";
+        s = s.replace(".", "");
+        System.out.println(s);
+
+
+    }
+}
--- src/main/java/hu/nive/ujratervezes/wordcounter/WordCounter.java
+++ src/main/java/hu/nive/ujratervezes/wordcounter/WordCounter.java
@@ -1,5 +1,34 @@
 package hu.nive.ujratervezes.wordcounter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordCounter {
 
+    public int[] countWords(String fileName, int wordLength) {
+        List<String> rows = new ArrayList<>();
+        try {
+            rows = Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int[] countWord = new int[rows.size()];
+        int index = 0;
+        for (String row : rows) {
+            row = row.replace(".", "");
+            row = row.replace("!", "");
+            row = row.replace("?", "");
+            String[] words = row.split(" ");
+            for (String s : words) {
+                if (s.length() == wordLength) {
+                    countWord[index] += 1;
+                }
+            }
+            index++;
+        }
+        return countWord;
+    }
 }
